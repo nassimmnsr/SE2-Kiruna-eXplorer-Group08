ac4830ea6812cf6e85be6a9f23c842d339ce639e
RUGGERO ROSSINO s323698 <s323698@studenti.polito.it>
2024-11-02 18:51:25 +0100

    Fix: added the possibility to define multiple routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,29 @@
 // App.js
-import { Container } from 'react-bootstrap';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ListDocuments from './components/ListDocuments';
+import { Container } from "react-bootstrap";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ListDocuments from "./components/ListDocuments";
 
-const App = () => (
-  <Container fluid className="d-flex flex-column min-vh-100 p-0">
-    <Header />
-    <ListDocuments/>
-    <Footer />
-  </Container>
-);
+function App() {
+  return (
+    <Routes>
+      <Route
+        element={
+          <>
+            <Header />
+            <Container fluid className="d-flex flex-column min-vh-100 p-0">
+              <Outlet />
+            </Container>
+            <Footer />
+          </>
+        }
+      >
+        <Route path="/" element={<ListDocuments />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Route>
+    </Routes>
+  );
+}
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([{path: "/*", element:<App/>}]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
