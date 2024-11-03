f20cd95afcfe6c2f63078ecb9d9e3e87a4cc1e86
Alessandro Fedriga <s320136@studenti.polito.it>
2024-11-03 21:12:59 +0100

    swapped type from integer to long

--- backend/src/main/java/com/kirunaexplorer/app/repository/DocumentRepository.java
+++ backend/src/main/java/com/kirunaexplorer/app/repository/DocumentRepository.java
@@ -3,5 +3,5 @@ package com.kirunaexplorer.app.repository;
 import com.kirunaexplorer.app.model.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DocumentRepository extends JpaRepository<Document, Integer> {
+public interface DocumentRepository extends JpaRepository<Document, Long> {
 }
